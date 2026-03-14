5e40d0d88a6cf3c7c5c6823268b08f1ab93ec2ac
Valeria Lopez <DValeriaLopez@users.noreply.github>
2026-03-13 19:59:22 -0500

    Implementación de búsqueda lineal en Searching.cpp

--- src/algorithms/Searching.hpp
+++ src/algorithms/Searching.hpp
@@ -2,7 +2,15 @@
 #include "../models/Product.hpp"
 
 // Búsqueda lineal en arreglo
-Product* linearSearch(Product arr[], int n, int codigo);
+Product* linearSearch(Product arr[], int n, int codigo){
+  for (int i = 0; i < n; i++) {
+        if (arr[i].codigo == codigo) {
+            return &arr[i]; 
+        }
+    }
+    return nullptr; 
+
+};
 
 // Búsqueda binaria en arreglo ordenado
 Product* binarySearch(Product arr[], int n, int codigo);
