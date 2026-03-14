e2f8765446e8d16c2a54a1cd85e95047fde3e2bd
vienheno <herrera.victor@outlook.com>
2026-03-13 22:24:44 -0500

    Nueva version

--- src/algorithms/Searching.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../models/Product.hpp"
-
-// Búsqueda lineal en arreglo
-Product* linearSearch(Product arr[], int n, int codigo);
-
-// Búsqueda binaria en arreglo ordenado
-Product* binarySearch(Product arr[], int n, int codigo);
